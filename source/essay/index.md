bd30ea62162569b9ee2f997ff49b36c2e91840fc
nibabashilkk <1449584369@qq.com>
2022-12-27 17:07:58 +0800

    开始

--- source/essay/index.md
+++
@@ -1,8 +0,0 @@
----
-title: 即刻短文
-date: 2022-12-26 21:19:44
-comments: true
-aside: false
-top_img: false
-type: essay
----
--- themes/butterfly/source/js/suitpjax.js
+++
@@ -1,7 +0,0 @@
-function whenDOMReady() {
-//此处填写需要执行的代码，如:
-get_name();
-}
-
-whenDOMReady() //打开网站之后先执行一次函数
-document.addEventListener(“pjax:complete”, whenDOMReady)
