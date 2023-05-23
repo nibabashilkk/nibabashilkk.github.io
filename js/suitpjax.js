047f6ef20c999f626ef30d145f063c295c4e877f
lkk <1449584369@qq.com>
2023-05-23 18:56:35 +0800

    start

--- js/suitpjax.js
+++
@@ -1,7 +0,0 @@
-function whenDOMReady() {
-//此处填写需要执行的代码，如:
-document.getElementById("page-name").innerText = document.title.split("| 爱加班的小刘")[0];
-}
-
-whenDOMReady() //打开网站之后先执行一次函数
-document.addEventListener("pjax:complete", whenDOMReady) //pjax加载完成之后执行上面函数
